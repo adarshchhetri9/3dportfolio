c1d9e309b00ac6b6798f2fb78887fa86d0fe465d
adarshchhetri9 <chhetriadarsh912@gmail.com>
2023-03-05 11:57:42 +0530

    middle of about

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,9 +1,38 @@
-import React from 'react'
+import React from "react";
+import Tilt from "react-tilt";
+import { motion } from "framer-motion";
+import { styles } from "../style";
+import { services } from "../constants";
+import { fadeIn, textVariant } from "../utils/motion";
+
+const ServiceCard = ({ index, title, icon }) => {
+  return <p>{title}</p>;
+};
 
 const About = () => {
   return (
-    <div>About</div>
-  )
-}
+    <>
+      <motion.div variants={textVariant()}>
+        <p className={styles.sectionSubText}> Introduction</p>
+        <h2 className={styles.sectionHeadText}>Overview.</h2>
+      </motion.div>
+      <motion.p
+        varieants={fadeIn("", "", 0.1, 1)}
+        className="mt-4 text-secondary  text-[17px] max-w-3xl leading-[30px]"
+      >
+        I'm a skilled software developer with experience in JavaScript, and
+        expertise in frameworks like React and Node.js. I'm a quick learner and
+        collaborate closely with clients to create efficient, scalable, and
+        user-friendly solutions that solve real-world problems. Let's work
+        together to bring your ideas to life!
+      </motion.p>
+      <div className="mt-20p flex flex-wrap gap-10">
+        {services.map((service, index) => (
+          <ServiceCard key={service.title} index={index} {...service} />
+        ))}
+      </div>
+    </>
+  );
+};
 
-export default About
+export default About;
